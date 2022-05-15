53ce5ca303d153ce482969c282d8cefbb586bd08
Alexey Komar <pikachuna@yandex.ua>
2022-05-15 16:57:54 +0300

    Fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ editButton.addEventListener('click', function() {
     popup.classList.add('popup_opened');
 });
 
-let popupClose = popup.querySelector('.popup__container_close');
+let popupClose = popup.querySelector('.popup-container__close');
 popupClose.addEventListener('click', function() {
     popup.classList.remove('popup_opened');
 });
@@ -22,7 +22,7 @@ let profileSub = profileInfo.querySelector('.Profile__info_subtitle');
 
 
 
-let formElement = popup.querySelector('.popup__container_form');
+let formElement = popup.querySelector('.popup-form');
 let input = formElement.querySelector('.popup__container_field');
 let inputSub = formElement.querySelector('.popup__container_field_sub');
 input.setAttribute('value', profileTitle.textContent);
