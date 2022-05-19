74b96e8d3a05e4cac7e9f02eb092b89a161c3a78
Alexey Komar <pikachuna@yandex.ua>
2022-05-19 18:41:43 +0300

    Fix

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const popup = document.querySelector('.popup');
+const profileTitle = document.querySelector('.profile-info__title');
+const profileSub = document.querySelector('.profile-info__subtitle');
+const editButton = document.querySelector('.profile-info__button');
+const formElement = popup.querySelector('.popup-form');
+const formButton = formElement.querySelector('.popup-form__button')
+const inputTitle = formElement.querySelector('.popup-form__field');
+const inputSub = formElement.querySelector('.popup-form__field_type_sub');
+const closeButton = popup.querySelector('.popup-container__close');
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = inputTitle.value;
+    profileSub.textContent = inputSub.value;
+    popup.classList.remove('popup_opened');
+}
+
+function popupOpen() {
+    popup.classList.add('popup_opened');
+    inputTitle.setAttribute('value', profileTitle.textContent);
+    inputSub.setAttribute('value', profileSub.textContent);
+}
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+formButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
