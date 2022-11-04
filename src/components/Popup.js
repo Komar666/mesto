1a0c5173d4f96acb0ea7c17c2c0ec8fb7bf3282e
Alexey Komar <pikachuna@yandex.ua>
2022-11-04 03:14:18 +0300

    Fix

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -23,7 +23,7 @@ export default class Popup {
     setEventListeners() {
         this._popup.addEventListener('click', (evt) => {
 
-            if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup-container__close')) {
+            if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup-container-close')) {
                 this.close();
             }
         });
